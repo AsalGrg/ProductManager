624ae32dc67f462e067b379a9fd6bcfafe234000
asalg <gurung.asal.06@gmail.com>
2026-06-15 15:23:54 +0545

    changing deployed models urls

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://productify-wuhv.onrender.com",
   withCredentials: true, // important for cookies
   headers: {
     "Content-Type": "application/json",
